dd6989f52ba05d6849285860c29c0bcdd320d3eb
hiennguyen0412 <nguyenhien4120@gmail.com>
2018-03-04 10:03:37 +0700

    fix servlet

--- src/controller/LoginServlet.java
+++ src/controller/LoginServlet.java
@@ -5,22 +5,21 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.io.PrintWriter;
 
-
-@WebServlet(name = "controller.LoginServlet")
-public class LoginServlet extends HttpServlet{
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+@WebServlet(name = "LoginServlet")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
             PrintWriter out = response.getWriter();
             response.sendRedirect("index.jsp");
         }catch (Exception e){
 
         }
-
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 }
